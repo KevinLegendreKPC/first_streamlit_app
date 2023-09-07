2c1db98654935ab49ffcf5103872dba096eedb9c
KevinLegendreKPC <144346893+KevinLegendreKPC@users.noreply.github.com>
2023-09-07 14:42:00 +0200

    Update streamlit_app.py
    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit 
 
-streamlit.title('"My Mom's new Healthy Diner"')
+streamlit.title('My Mom new Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text ('🥑🍞 Avocado Toast')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
    
